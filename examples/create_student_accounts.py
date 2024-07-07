8de5543093d62db8af276b241d8a16f0a2bcc655
j-hudecek <j.hudecek@nki.nl>
2024-07-07 18:51:59 +0200

    create students account example

---
+++ examples/create_student_accounts.py
@@ -0,0 +1,29 @@
+
+import sys
+import os
+
+import slidescore
+
+# Either set the environment variables, or hardcode your settings below
+SLIDESCORE_API_KEY = os.getenv('SLIDESCORE_API_KEY') or input('What is your Slidescore API key: ') # eyb..
+SLIDESCORE_HOST = os.getenv('SLIDESCORE_HOST') or input('What is your Slidescore host: ') # https://slidescore.com/
+SLIDESCORE_EMAIL = os.getenv('SLIDESCORE_EMAIL') or input('What is your Slidescore email: ') # test@example.com
+
+if __name__ == "__main__":
+    # Check the login credentials and create an API client
+    if not SLIDESCORE_API_KEY or not SLIDESCORE_HOST:
+        sys.exit('SLIDESCORE_API_KEY or SLIDESCORE_HOST not set, please set these variables for your setup')
+    # Remove "/" suffix if needed
+    SLIDESCORE_HOST = SLIDESCORE_HOST[:-1] if SLIDESCORE_HOST.endswith('/') else SLIDESCORE_HOST
+
+    client = slidescore.APIClient(SLIDESCORE_HOST, SLIDESCORE_API_KEY)
+    print('Created API client')
+    
+    class_id = int(input("What is the class id: "))
+
+
+    for i in range(10,95):
+        print(client.perform_request("GenerateStudentAccount", {
+                 "username": "acc"+str(i)+"@v", #email-like identifier
+                 "classID": class_id
+                 }, method="POST").text)
